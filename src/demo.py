dd35f518b85fff29a8c34b9c5de07ac1166684c1
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2021-01-16 17:44:12 +0000

    [NEW] Demo Source Code

--- src/demo.py
+++ src/demo.py
@@ -22,6 +22,48 @@ __credits__     = [
 	"Nuno Nunes"
 ]
 
-import pydicom
+import sys, os.path
+import matplotlib.pyplot as plt
+from pydicom import dcmread
+from pydicom.data import get_testdata_file
+
+pathDirname = os.path.dirname(__file__)
+joinPath = os.path.join(pathDirname, '..', '..', '..')
+pathAbsPath = os.path.abspath(joinPath)
+
+repoJoinPath = os.path.join(pathAbsPath, 'Git', 'dataset-uta7-dicom')
+sys.path.append(repoJoinPath)
+repoAbsPath = os.path.abspath(repoJoinPath)
+
+datasetJoinPath = os.path.join(repoAbsPath, 'dataset', 'Anonymized1')
+sys.path.append(datasetJoinPath)
+datasetAbsPath = os.path.abspath(datasetJoinPath)
+
+fileNameToRead = "MG000000.dcm"
+fullPathToFile = os.path.join(datasetJoinPath, fileNameToRead)
+
+ds = dcmread(fullPathToFile)
+
+# Normal mode:
+print()
+print(f"File path........: {fullPathToFile}")
+print(f"SOP Class........: {ds.SOPClassUID} ({ds.SOPClassUID.name})")
+print()
+
+pat_name = ds.PatientName
+display_name = pat_name.family_name + ", " + pat_name.given_name
+print(f"Patient's Name...: {display_name}")
+print(f"Patient ID.......: {ds.PatientID}")
+print(f"Modality.........: {ds.Modality}")
+print(f"Study Date.......: {ds.StudyDate}")
+print(f"Image size.......: {ds.Rows} x {ds.Columns}")
+print(f"Pixel Spacing....: {ds.PixelSpacing}")
+
+# use .get() if not sure the item exists, and want a default value if missing
+print(f"Slice location...: {ds.get('SliceLocation', '(missing)')}")
+
+# plot the image using matplotlib
+plt.imshow(ds.pixel_array, cmap=plt.cm.gray)
+plt.show()
 
 # ==================== END File ==================== #
